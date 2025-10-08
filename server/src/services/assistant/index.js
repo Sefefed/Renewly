ea628a0d6174d161f99150757cb26aab38e8c378
Sefefe <sefefedagne6@gmail.com>
2025-10-08 10:35:29 +0300

    chore: cleanup unused assets and docs

--- server/src/services/assistant/index.js
+++
@@ -1,3 +0,0 @@
-export { default as financialAssistant } from "./financialAssistantService.js";
-export { default as personalizationEngine } from "./personalizationEngine.js";
-export { default as llmProvider } from "./llmProvider.js";
